078459c506e6a35763d932256db22b8350e89172
Sachin Saini <sachin17897@gmail.com>
2017-09-03 17:08:35 +0530

    Practicing Forms and 2 way data binding

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -26,18 +26,18 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent{
-    name = 'Sachin Saini';
-    age = 20;
+    name:string = 'Sachin Saini';
+    age:number = 20;
     isGreen = true;
     isUpper = true;
     currentClasses = {};
     currentStyles = {};
-    cake='britannia';
+    // cake='britannia';
     birthday = new Date(1997,7,17);
 
     imageUrl = 'http://lorempixel.com/400/200';
-    showName:boolean = true;
-    switchNo:number = 5;
+    // showName:boolean = true;
+    // switchNo:number = 5;
     people = ['john', 'doe', 'naruto', 'uzumaki'];
     peopleObjectArray = [
         {
@@ -65,7 +65,7 @@ export class SandboxComponent{
     constructor(){
         console.log('constructor ran...');
         this.person.age=21;
-        this.customerProperties();
+        //this.customerProperties();
         this.currentClasses = {
             isGreen:true,
             isUpper:true
@@ -78,43 +78,43 @@ export class SandboxComponent{
         }
     }
 
-    showAge(){
-        return this.person.age+10;
-    }
+    // showAge(){
+    //     return this.person.age+10;
+    // }
 
-    customerProperties()
-    {
-        this.customer = {
-            id:1,
-            name:'sachin',
-            email:'haha@gmail.com'
-        }
+    // customerProperties()
+    // {
+    //     this.customer = {
+    //         id:1,
+    //         name:'sachin',
+    //         email:'haha@gmail.com'
+    //     }
 
-        return this.customer;
-    }
-    myStr:string = "Hello World";
-    boolValue:boolean = true;
-    fireEvent(e)
-    {
-        // console.log("fired a click event wooohoo");
-        // console.log(message);
-        // console.log(e);
-        // console.log(e.target.id);
-        console.log(e.type);
-    }
-    changeValue(e)
-    {
-        this.myStr = "Goodbye World";
-        //e.myStr = "bas bas";
-        this.boolValue = !this.boolValue;
-        console.log(e);    
-    }
-    myChangedValue = 'Hello';
-    keyEvent(e)
-    {
-        this.myChangedValue = e.target.value;
-        console.log(e.type);
-    }
+    //     return this.customer;
+    // }
+    // myStr:string = "Hello World";
+    // boolValue:boolean = true;
+    // fireEvent(e)
+    // {
+    //     // console.log("fired a click event wooohoo");
+    //     // console.log(message);
+    //     // console.log(e);
+    //     // console.log(e.target.id);
+    //     console.log(e.type);
+    // }
+    // changeValue(e)
+    // {
+    //     this.myStr = "Goodbye World";
+    //     //e.myStr = "bas bas";
+    //     this.boolValue = !this.boolValue;
+    //     console.log(e);    
+    // }
+    // myChangedValue = 'Hello';
+    // keyEvent(e)
+    // {
+    //     this.myChangedValue = e.target.value;
+    //     console.log(e.type);
+    // }
 }
 
 
