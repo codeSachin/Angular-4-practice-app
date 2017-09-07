9e4bb42d516955f9f69455ffa27822f3160f6e28
Sachin Saini <sachin17897@gmail.com>
2017-09-07 16:57:38 +0530

    practicing HTTP GET rrquest

--- src/app/components/mysandbox/mysandbox.component.ts
+++ src/app/components/mysandbox/mysandbox.component.ts
@@ -1,15 +1,51 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 
-
 @Component({
-  selector: 'mysandbox',
-  // templateUrl: './mysandbox.component.html',
-  template:`
- 
-  `,
-  styleUrls: ['./mysandbox.component.css']
+  selector:"mysandbox",
+  /*template:`
+  <div class="container">
+    <ul class="list-group">
+      <li class="list-group-item" *ngFor="let d of data"> {{d}} </li>
+    </ul>
+
+    <div *ngFor = "let user of users">
+      <h6> User Name: {{user.name}} </h6>
+      <p> Use Email :{{user.email}} </p>
+    </div>
+  </div>
+*/
+  templateUrl:"./mysandbox.component.html"
+  
 })
+
 export class MysandboxComponent{
- 
+
+  data:any[] = [];
+  users:any[] =[];
+  name:string = 'Sachin Saini';
+  age:number = 20;
+  user = {
+    name:'',
+    email:'',
+    phone:''
+  }
+  constructor(public dataservice:DataService){
+    this.dataservice.getData().subscribe((data) => {
+      console.log(data);
+      this.data.push(data);  //used push function here because we created out own observable return which returns after every one second. so we need to push to our data array every time we get a response data.
+        
+    });
+    this.dataservice.getUsers().subscribe((data)=>{
+      console.log(data);
+      this.users = data; //we directly pasted the data into our users array. because online API delivers direct data simultaneously.
+    });
+  }
+  submitted(){
+    this.dataservice.addUser(this.user).subscribe((user) => {
+      console.log(user);
+      this.users.unshift(user);
+    });
+  }
+
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class DataService {
-  
-  
+export class DataService{
+  data: Observable<any>;
+
+  constructor(public http:Http){
+
+  }
+
+  getUsers(){
+
+    return this.http.get('http://jsonplaceholder.typicode.com/users')
+    .map(res => res.json());
+  }
+  addUser(user){
+    return this.http.post('http://jsonplaceholder.typicode.com/users',user)
+    .map(res => res.json());
+  }
+
+  getData(){
+
+    this.data = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(1);
+      },1000);
+
+      setTimeout(()=> {
+        observer.next(2);
+      },2000);
+
+      setTimeout(()=> {
+        observer.next(3);
+      },3000);
+
+      setTimeout(()=> {
+        observer.next(4);
+      },4000);
+
+      setTimeout(()=> {
+        observer.next('Hello');
+      },5000);
+
+      setTimeout(()=> {
+        observer.complete();
+      },6000);
+
+
+    });
+    return this.data;
+  }
 }
